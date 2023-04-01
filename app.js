68fb73ccc316020aa8fe95e5551dfab16d1d42b2
Jaseel <jaseeleranhikkunnu@gmail.com>
2023-04-01 23:58:13 +0530

    used fileUpload()

--- app.js
+++ app.js
@@ -25,6 +25,8 @@ app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
+app.use(fileUpload())
+
 app.use('/', userRouter);
 app.use('/admin', adminRouter);
 
