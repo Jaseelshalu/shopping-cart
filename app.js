c56da883b9638cbdf32a9d7b4a21df3c5b549246
Jaseel <jaseeleranhikkunnu@gmail.com>
2023-04-01 09:24:20 +0530

    hbs module imported in variable 'hbs'

--- app.js
+++ app.js
@@ -7,6 +7,8 @@ var logger = require('morgan');
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
 
+var hbs = require('express-handlebars') //new
+
 var app = express();
 
 // view engine setup
