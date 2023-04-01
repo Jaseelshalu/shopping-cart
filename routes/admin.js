cfbb3971cee6935744b046cf77a7e6418839844b
Jaseel <jaseeleranhikkunnu@gmail.com>
2023-04-01 22:57:44 +0530

    add-product path setted

--- routes/admin.js
+++ routes/admin.js
@@ -34,4 +34,8 @@ router.get('/', function (req, res, next) {
   res.render('admin/view-products', { admin: true, products });
 });
 
+router.get('/add-product',(req,res)=>{
+  res.render('admin/add-product')
+})
+
 module.exports = router;
