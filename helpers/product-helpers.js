00ac10d6acd9a63ef45f38bb898d7a9072270d1f
Jaseel <jaseeleranhikkunnu@gmail.com>
2023-04-02 22:36:52 +0530

    callback function removed and used promise

--- helpers/product-helpers.js
+++ helpers/product-helpers.js
@@ -1,9 +1,11 @@
 var db = require('../config/connection')
 var collection = require('../config/collection')
-module.exports={
-    addProduct:(product,callback)=>{
-        db.get().collection(collection.PRODUCT_COLLECTION).insertOne(product).then((data)=>{
-            callback(data.insertedId)
+module.exports = {
+    addProduct: (product) => {
+        return new Promise((resolve, reject) => {
+            db.get().collection(collection.PRODUCT_COLLECTION).insertOne(product).then((data) => {
+                resolve(data)
+            })
         })
     }
 }
