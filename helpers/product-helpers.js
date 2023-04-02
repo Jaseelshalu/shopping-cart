2b1cdffd7a50ece5e1790af43767ef8682483ab3
Jaseel <jaseeleranhikkunnu@gmail.com>
2023-04-02 22:23:02 +0530

    PRODUCT_COLLECTION used

--- helpers/product-helpers.js
+++ helpers/product-helpers.js
@@ -2,7 +2,7 @@ var db = require('../config/connection')
 var collection = require('../config/collection')
 module.exports={
     addProduct:(product,callback)=>{
-        db.get().collection('product').insertOne(product).then((data)=>{
+        db.get().collection(collection.PRODUCT_COLLECTION).insertOne(product).then((data)=>{
             callback(data.insertedId)
         })
     }
