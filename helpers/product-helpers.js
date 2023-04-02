e9c3f3e127f620b37bf6cd8ab1be2bc11eb1d8ae
Jaseel <jaseeleranhikkunnu@gmail.com>
2023-04-02 14:51:37 +0530

    consoled data of product inserted

--- helpers/product-helpers.js
+++ helpers/product-helpers.js
@@ -1,6 +1,8 @@
 var db = require('../config/connection')
 module.exports={
     addProduct:(product)=>{
-        db.get().collection('product').insertOne(product)
+        db.get().collection('product').insertOne(product).then((data)=>{
+            console.log(data);
+        })
     }
 }
