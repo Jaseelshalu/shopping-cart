8e8f743d7b58283a8533629d81329c901e3852bc
Jaseel <jaseeleranhikkunnu@gmail.com>
2023-04-02 14:52:31 +0530

    removed consoling data of product inserted

--- helpers/product-helpers.js
+++ helpers/product-helpers.js
@@ -2,7 +2,7 @@ var db = require('../config/connection')
 module.exports={
     addProduct:(product)=>{
         db.get().collection('product').insertOne(product).then((data)=>{
-            console.log(data);
+            
         })
     }
 }
