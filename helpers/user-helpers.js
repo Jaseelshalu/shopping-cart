aa3ea8421237e164c78868cd69beaa35860d3c6a
Jaseel <jaseeleranhikkunnu@gmail.com>
2023-04-03 19:48:27 +0530

    doSignup function setted up

--- helpers/user-helpers.js
+++ helpers/user-helpers.js
@@ -1,9 +1,13 @@
 var db = require('../config/connection')
 var collection = require('../config/collection')
+var bcrypt = require('bcrypt')
 module.exports = {
-    doSigup: () => {
-        return new Promise((resolve, reject) => {
-            
+    doSigup: (userData) => {
+        return new Promise(async (resolve, reject) => {
+            userData.Password = await bcrypt.hash(userData.Password, 10)
+            db.get().collection(collection.USER_COLLECTION).insertOne(userData).then(() => {
+                resolve()
+            })
         })
     }
 }
