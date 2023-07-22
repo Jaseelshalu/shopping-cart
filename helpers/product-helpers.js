cf43b826c4e740c5025e02d81650f50ad90a7e69
Jaseelshalu <123951774+Jaseelshalu@users.noreply.github.com>
2023-07-22 07:09:45 +0530

    Update product-helpers.js

--- helpers/product-helpers.js
+++ helpers/product-helpers.js
@@ -1,16 +1,9 @@
 var db = require('../config/connection')
 // var collection = require('../config/collection')
-const collection = {
-    PRODUCT_COLLECTION: 'product',
-    USER_COLLECTION: 'user',
-    CART_COLLECTION: 'cart',
-    ORDER_COLLECTION: 'order',
-    ADMIN_COLLECTION: 'admin'
-};
 module.exports = {
     addProduct: (product) => {
         return new Promise((resolve, reject) => {
-            db.get().collection(collection.PRODUCT_COLLECTION).insertOne(product).then((data) => {
+            db.get().collection('product').insertOne(product).then((data) => {
                 resolve(data.insertedId)
             })
         })
